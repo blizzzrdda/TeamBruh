f9679f441a543c23b957d2c2ffcaaa6935aad25f
King Zhou <zhouyimingking@outlook.com>
2022-01-22 17:21:06 -0800

    Add freelook camera, made playershadow prefab

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,11 +1,14 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
     public static InputManager Instance { get; set; }
+    public bool isControllingReal;
+    
     private PlayerInputMap _playerInputMap;
-    private InputAction _moveHorizontal;
+    private InputAction _moveHorizontal, _moveVertical;
 
     private void Awake()
     {
@@ -21,21 +24,25 @@ public class InputManager : MonoBehaviour
         _playerInputMap = new PlayerInputMap();
     }
 
+    private void Start()
+    {
+        isControllingReal = false;
+    }
+
     private void OnEnable()
     {
         _moveHorizontal = _playerInputMap.PlayerControl.MoveHorizontal;
         _moveHorizontal.Enable();
 
-        _playerInputMap.PlayerControl.MoveUp.Enable();
-        _playerInputMap.PlayerControl.MoveDown.Enable();
+        _moveVertical = _playerInputMap.PlayerControl.MoveVertical;
+        _playerInputMap.PlayerControl.MoveVertical.Enable();
         _playerInputMap.PlayerControl.Jump.Enable();
     }
 
     private void OnDisable()
     {
         _moveHorizontal.Disable();
-        _playerInputMap.PlayerControl.MoveUp.Disable();
-        _playerInputMap.PlayerControl.MoveDown.Disable();
+        _playerInputMap.PlayerControl.MoveVertical.Enable();
         _playerInputMap.PlayerControl.Jump.Disable();
     }
 
@@ -44,19 +51,9 @@ public class InputManager : MonoBehaviour
         return _moveHorizontal.ReadValue<float>();
     }
 
-    public int GetMoveVerticalThisFrame()
+    public float GetMoveVertical()
     {
-        if (_playerInputMap.PlayerControl.MoveUp.triggered && !_playerInputMap.PlayerControl.MoveDown.triggered)
-        {
-            return 1;
-        }
-
-        if (!_playerInputMap.PlayerControl.MoveUp.triggered && _playerInputMap.PlayerControl.MoveDown.triggered)
-        {
-            return -1;
-        }
-
-        return 0;
+        return _moveVertical.ReadValue<float>();
     }
 
     public bool GetJumpThisFrame()
