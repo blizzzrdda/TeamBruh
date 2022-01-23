bb952099b08b05614fb9fe43c0949126eed8121b
DAHAI LI <blizzzrdda@gmail.com>
2022-01-22 22:52:53 -0500

    + Added level change + Added some level templates

--- Assets/Scripts/ShapeConstructor.cs
+++ Assets/Scripts/ShapeConstructor.cs
@@ -7,24 +7,23 @@ public class ShapeConstructor : MonoBehaviour
 {
     [SerializeField] private List<Transform> vertices;
 
-    // private void OnDrawGizmos()
-    // {
-    //     if(vertices.Count != 4) return;
-    //     
-    //     Mesh mesh = new Mesh();
-    //
-    //     Vector3[] verticesToProject = new Vector3[4];
-    //
-    //     verticesToProject[0] = vertices[0].localPosition;
-    //     verticesToProject[1] = vertices[1].localPosition;
-    //     verticesToProject[2] = vertices[2].localPosition;
-    //     verticesToProject[3] = vertices[3].localPosition;
-    //
-    //     mesh.vertices = verticesToProject;
-    //     
-    //     mesh.triangles = new int[] {0, 1, 2, 0, 2, 3};
-    //
-    //     Graphics.DrawMeshNow(mesh,
-    //         Matrix4x4.TRS(Vector3.zero, Quaternion.identity, Vector3.one));
-    // }
+    private void OnDrawGizmos()
+    {
+        if(vertices.Count != 4) return;
+        
+        Mesh mesh = new Mesh();
+    
+        Vector3[] verticesToProject = new Vector3[4];
+    
+        verticesToProject[0] = vertices[0].localPosition;
+        verticesToProject[1] = vertices[1].localPosition;
+        verticesToProject[2] = vertices[2].localPosition;
+        verticesToProject[3] = vertices[3].localPosition;
+    
+        mesh.vertices = verticesToProject;
+        
+        mesh.triangles = new int[] {0, 1, 2, 0, 2, 3};
+    
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
 }
